6f9723cd3f150b8cc79f2a08249dd77a7384d7c8
Jonathan <33232878+unlikelykoala@users.noreply.github.com>
2024-12-08 20:25:12 -0500

    start p2

---
+++ p2_point_mutations.py
@@ -0,0 +1,33 @@
+'''
+notes:
+    - mutation: mistale that occurs during copying/creation of a nucleic acid, esp DNA
+    - point mutation: replaces one base with another at a single nucelotide
+        - Hamming distance: minimum number of point mutations that occurred during evolution betweeen the two strands
+            - the diff between two homologous DNA strands from diff genomes with a common ancestor
+            - only defined for seqs of equal length
+            - if unequal length, compute hamming distance over the shorter length
+
+input: make a DNA class that passes the tests
+
+rules:
+    - make a DNA class
+        - iv: 
+            - stand is a string
+        - methods:
+            - hamming_distance(string_of_other_strand)
+                - if one is longer, only compare up to the length of that strand. the additional bases are simply ignored and not counted as mutations
+
+
+DS:
+    - 
+
+Alg:
+    - 
+
+'''
+class DNA:
+    def __init__(self, strand):
+        self.strand = strand
+
+    def hamming_distance(self, other):
+        pass
